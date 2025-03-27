e09948427e76f50ea356a9a03def56a6647bff16
JayMurphy45 <B00143682@mytudublin.ie>
2025-03-27 09:32:35 +0000

    fixed index bug and added main

--- SmallestMissingInteger/SmallestMissingInteger.py
+++ SmallestMissingInteger/SmallestMissingInteger.py
@@ -5,7 +5,7 @@ class Solution:
     def missingInteger(self, nums: List[int]) -> int:
         count = nums[0] # set the count to the first element in the list
         i = 0 # set the index to 0
-        while i < len(nums):
+        while i < len(nums) - 1:
             i += 1
             if nums[i]==nums[i-1]+1:
                 count+= nums[i]
@@ -16,3 +16,15 @@ class Solution:
                 return count
             else:
                 count+=1
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    x = [1, 2, 3, 4, 6, 7, 8, 9, 10]
+    y = [1, 3, 4, 5, 6, 7, 8, 9, 10]
+    z = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    print(s.missingInteger(x))
+    print(s.missingInteger(y))
+    print(s.missingInteger(z))
